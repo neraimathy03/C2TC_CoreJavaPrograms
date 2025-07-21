5d0ac3c79825dc46e5ff627c340c4c1a23b273bf
nerai <nerai@Sakthi-LAPTOP>
2025-07-22 00:26:51 +0530

    Initial Commit.

---
+++ C2TC_CoreJava/src/Day1/DataTypesDemo.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		int value1 = 4 / 3; //integer division
+		float value2 = 100 / 3;
+		double value3 = 4d / 3d;
+		float value4 = 5/2.5f; //float division
+		System.out.println("value 1= " + value1); 
+		System.out.println("value 2= " + value2);
+		System.out.println("value 3= " + value3);
+		System.out.println("value 4= " + value4);	
+		int marker = 500;
+		//assigning expression to variable 
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+	}
+
+}
